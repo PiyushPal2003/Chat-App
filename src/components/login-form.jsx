da6787bfc3af4032d7b8e3c105b1ffbf28dc2b90
PiyushPal <piyushpal09255@gmail.com>
2025-07-23 23:26:33 +0530

    auth page validation and show/hide password

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -10,9 +10,14 @@ export function LoginForm({
 }) {
 
   const [authState, setAuthState] = useState('Login');
+  const [passVisible, setPassVisible] = useState(false);
 
   function changeAuthState() {
     setAuthState(authState === 'Login' ? 'Sign up' : 'Login');
+  }
+
+  function toggleEye() {
+    setPassVisible((prev) => !prev);
   }
 
   return (
@@ -24,6 +29,12 @@ export function LoginForm({
         </p>
       </div>
       <div className="grid gap-6">
+        {authState == 'Sign up' && (
+          <div className="grid gap-3">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" type="text" placeholder="Name" pattern="^[A-Za-z]{1,10}$" title="Alphabets only, upto 10 characters" required />
+          </div>
+        )}
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
           <Input id="email" type="email" placeholder="m@example.com" required />
@@ -35,7 +46,28 @@ export function LoginForm({
               Forgot your password?
             </a>
           </div>
-          <Input id="password" type="password" required />
+
+          <div className="flex items-between">
+            <Input id="password" type={passVisible ? "text" : "password"} pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{1,}$" title="Must contain Uppercase, Lowercase and Numbers" required />
+
+            {
+              passVisible ? (
+                <button className="open-eye" id="open-eye" onClick={toggleEye}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  </svg>
+                </button>
+              ):(
+                <button className="close-eye" id="close-eye" onClick={toggleEye}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  </svg>
+                </button>
+              )
+            }
+          </div>
+
         </div>
         <Button type="submit" className="w-full">
           {authState}
@@ -50,11 +82,11 @@ export function LoginForm({
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
           <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
           </svg>
-          {authState} with Google
+          Continue with Google
         </Button>
       </div>
       <div className="text-center text-sm">
-        {authState == 'Login' ? `Don't have an account?` : `Already have an account?`}
+        {authState == 'Login' ? `Don't have an account? ` : `Already have an account? `}
         <a href="#" className="underline underline-offset-4" onClick={changeAuthState}>
           {authState == 'Login' ? 'Sign up' : 'Login'}
         </a>
