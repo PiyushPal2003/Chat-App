77282f747b451bf9dcbacf913b0c0ec016ac6c8d
PiyushPal <piyushpal09255@gmail.com>
2025-09-02 00:11:44 +0530

    trying to show chat list

--- src/component/Chats/Chats.jsx
+++ src/component/Chats/Chats.jsx
@@ -6,24 +6,27 @@ import { useSelector } from 'react-redux';
 export default function Chats() {
 
     const user = useSelector((state)=>state.auth);
-    if(user?.id){
-        const { data, error, isLoading, isSuccess, refetch } = useGetChatsQuery(user.id);
-    }
+    const { data, error, isLoading, isSuccess, refetch } = useGetChatsQuery(user.id);
 
     useEffect(()=>{
         if(isSuccess){
             console.log("Chat data fetched successfully:", data);
         }
-    })
+        else if(error){
+            console.error("Error fetching chat data:", error);
+        }
+    }, [isSuccess, error]);
 
   return (
     <div>
         {
-            Array.from({length:6}).map((_, index)=>(
+            data?.chats?.map((chat, index)=>(
                 <div className='flex flex-row w-full h-[5rem] items-center mb-5 border rounded-full p-1' key={index} >
                     <img src='./assets/user_img.jpg' className='rounded-full object-cover h-4/5'/>
                     <div className='flex flex-col ml-2'>
-                        <h1 className='font-medium text-lg'>My Friend</h1>
+                        <h1 className='font-medium text-lg'>
+                            {chat?.members?.length<=2 ? chat?.members.filter((f)=>f._id !== user.id)?.name : 'Group Chat'}
+                        </h1>
                         <h1>Hi bro how are you??</h1>
                     </div>
                 </div>
